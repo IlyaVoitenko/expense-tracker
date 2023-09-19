c259fb1ddeae30b13f771cec84db28ca8cb57a14
illia Voitenko <illia@mbp-illia.localdomain>
2023-09-19 20:16:33 +0200

    added style for ItemUserBalance

--- src/components/UserBalance/ItemUserBalance/ItemUserBalance.js
+++ src/components/UserBalance/ItemUserBalance/ItemUserBalance.js
@@ -1,12 +1,16 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { filterIconBtn, filterStyleBtn } from "../../../utils/arrayUserBalance";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
 const ItemUserBalance = ({ title, summa }) => {
+  const icon = filterIconBtn(title);
+  const styleIcon = filterStyleBtn(title);
   return (
     <View style={styles.itemContainer}>
-      <Text>{title}</Text>
-      <Button title={""} />
-      <Text>{summa}</Text>
+      <Text style={styles.itemTitle}>{title}</Text>
+      <FontAwesomeIcon icon={icon} size={30} style={styleIcon} />
+      <Text style={styles.itemSumma}>{summa}</Text>
     </View>
   );
 };
@@ -17,7 +21,14 @@ const styles = StyleSheet.create({
   itemContainer: {
     justifyContent: "center",
     alignItems: "center",
+    marginTop: 15,
     width: 100,
     height: 80,
+  },
+  itemSumma: {
+    padding: 10,
+  },
+  itemTitle: {
+    paddingBottom: 5,
   },
 });
--- src/utils/arrayUserBalance/index.js
+++ src/utils/arrayUserBalance/index.js
@@ -1,8 +1,28 @@
 import { v4 as uuid } from "uuid";
-import { AiOutlineBank } from "react-icons/ai";
-import { BsWallet2 } from "react-icons/bs";
+import { faBuildingColumns, faWallet } from "@fortawesome/free-solid-svg-icons";
 
 export const userBalanceList = [
   { id: uuid(), title: "wallet", summa: 0 },
   { id: uuid(), title: "bank account", summa: 0 },
 ];
+
+export const filterIconBtn = (title) => {
+  switch (title) {
+    case "bank account":
+      return faBuildingColumns;
+    case "wallet":
+      return faWallet;
+    default:
+      break;
+  }
+};
+export const filterStyleBtn = (title) => {
+  switch (title) {
+    case "bank account":
+      return { color: "#3564b6" };
+    case "wallet":
+      return { color: "#00a4db" };
+    default:
+      break;
+  }
+};
